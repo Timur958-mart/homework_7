4f0a7acd3f8d399a5599b6c7433e2bd2e8869f1d
Timur958-mart <martusenko.timur@mail.ru>
2024-08-25 17:20:43 +0300

    Add files via upload

---
+++ .venv/module_7_4.py
@@ -0,0 +1,32 @@
+name1 = 'Мастера кода'
+name2 = "Волшебники данных"
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time)/tasks_total
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    result = (f"Победа команды {name1}!")
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    result = (f'Победа команды {name2}!')
+else:
+    result = "Ничья!"
+challenge_result = result
+
+# Использование %
+print("В команде %(name)s участников: %(team_num)s !" % {'name': name1, 'team_num': team1_num})
+print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
+
+# Использование format()
+print("Команда {} решила задач: {} !".format(name2, score_2))
+print("{} решили задачи за {} c !".format(name2, round(team2_time, 1)))
+
+# Использование f-строк
+print(f"Команды решили {score_1} и {score_2} задач.")
+print(f"Результат битвы: {challenge_result}")
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {round(time_avg, 1)} секунды на задачу!.")
